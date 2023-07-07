2334049b6886d4fe8b87494286e22fbeca9a57c8
shyamtiwari02062001 <shyam02062001@gmail.com>
2023-07-07 06:57:47 +0530

    UI for task 2 is ready

--- src/screens/Task2.js
+++ src/screens/Task2.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "./task2.css";
 const Task2 = () => {
   return (
-    <div>
-      <h1>Task2</h1>
+    <div className="task2Container">
+      <div className="cycleStarter">
+        <select className="dropdown">
+          <option>1 Cycles</option>
+          <option>2 Cycles</option>
+          <option>3 Cycles</option>
+        </select>
+        <button className="startButton">Start</button>
+      </div>
+      <center>
+        <span className="time">30:00</span>
+      </center>
     </div>
   );
 };
